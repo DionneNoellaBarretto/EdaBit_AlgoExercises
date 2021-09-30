492d96df529753ebfafa93beded175e36a19d9ce
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-09-29 20:38:49 -0400

    return+1toIntegerpassed

---
+++ ReturntheNextNumberfromtheIntegerPassed.js
@@ -0,0 +1,23 @@
+/*
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+Examples
+addition(0) ➞ 1
+
+addition(9) ➞ 10
+
+addition(-3) ➞ -2
+Notes
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+ */
+
+function addition(num) {
+	
+    return ++num;
+
+}
+console.log(addition(0));
+console.log(addition(9));
+console.log(addition(-3));
